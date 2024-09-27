92300ba81d4487aaaf05cef431d476e71b6f43fc
iKingNinja <83887130+iKingNinja@users.noreply.github.com>
2024-09-27 18:43:27 +0200

    Remove commented code

--- src/ServerScriptService/AccessManager/init.server.lua
+++ src/ServerScriptService/AccessManager/init.server.lua
@@ -3,7 +3,6 @@
 local API = require(game:GetService("ServerStorage").QACentralAccessManager)
 
 API.configuration:AllowRoles(API.configuration.roles.defaults.ALL_DEFAULTS)
--- API.configuration:AllowRoles({API.configuration.roles.defaults.QA_LEAD}, 1706973754 + 10000)
 API.configuration:SetAccessDeniedMessage("You do not have permission to join this game")
 
 game:GetService("Players").PlayerAdded:Connect(function(Player)
